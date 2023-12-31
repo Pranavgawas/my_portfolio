5bf16283854c7148e275e93816aa59a3f3d2c0a2
Pranavgawas <pranavgawas1999@gmail.com>
2023-12-31 17:57:37 +0530

    update css and header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -12,10 +12,10 @@ function Header() {
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#Education">Education</Nav.Link>
-                <Nav.Link href="#Project">Project</Nav.Link>
-                <Nav.Link href="#AboutMe">About Me</Nav.Link>
-                <Nav.Link href="#ContactMe">Contact Me</Nav.Link>
+                <Nav.Link href="./Welcome.js">Education</Nav.Link>
+                <Nav.Link href="./Welcome.js">Project</Nav.Link>
+                <Nav.Link href="./Welcome.js">About Me</Nav.Link>
+                <Nav.Link href="./Welcome.js">Contact Me</Nav.Link>
                 <img
             alt="Your Logo"
             src="https://i.pinimg.com/originals/34/33/1e/34331ee52a7229c33372739676e502bb.gif"  
