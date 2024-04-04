e52f42fc2d408e6c0093e29a54c48fe744fa2fdd
Pranav Gawas <pranavgawas1999@gmail.com>
2024-04-04 13:05:00 +0000

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './components/Hero.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <div data-theme="dracula">
+      <Hero />
+    </div>
+  )
+}
+
+export default App
